96edfa3ad9376e71429f1c78aaedfa41eb1770f4
tomoyaueno29 <u.t012807@gmail.com>
2020-07-08 14:17:29 +0900

    Update blockchain.go

--- blockchain.go
+++ blockchain.go
@@ -179,16 +179,12 @@ func main() {
 	blockchain.Print()
 
 	blockchain.AddTransaction("A", "B", 1.0)
-	previousHash := blockchain.LastBlock().Hash()
-	nonce := blockchain.ProofOfWork()
-	blockchain.CreateBlock(nonce, previousHash) //前のブロックのハッシュ値
+	blockchain.Mining()
 	blockchain.Print()
 
 	blockchain.AddTransaction("C", "D", 2.0)
 	blockchain.AddTransaction("X", "Y", 3.0)
-	previousHash = blockchain.LastBlock().Hash()
-	nonce = blockchain.ProofOfWork()
-	blockchain.CreateBlock(nonce, previousHash)
+	blockchain.Mining()
 	blockchain.Print()
 
 }
