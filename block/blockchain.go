750967a6be838391cf0dc4450f7c653e96bb243b
tomoyaueno29 <u.t012807@gmail.com>
2020-07-08 14:36:10 +0900

    Update blockchain.go

--- block/blockchain.go
+++ block/blockchain.go
@@ -1,4 +1,4 @@
-package main
+package block
 
 import (
 	"crypto/sha256"
@@ -184,28 +184,4 @@ func (t *Transaction) MarshalJSON() ([]byte, error) {
 		RecipientBlockchainAddress: t.recipientBlockchainAddress,
 		Value:                      t.value,
 	})
-}
-
-func init() {
-	log.SetPrefix("Blockchain:")
-}
-
-func main() {
-	myBlockchainAddress := "my_blockchain_address"
-
-	blockchain := NewBlockchain(myBlockchainAddress)
-	blockchain.Print()
-
-	blockchain.AddTransaction("A", "B", 1.0)
-	blockchain.Mining()
-	blockchain.Print()
-
-	blockchain.AddTransaction("C", "D", 2.0)
-	blockchain.AddTransaction("X", "Y", 3.0)
-	blockchain.Mining()
-	blockchain.Print()
-
-	fmt.Printf("my %.1f\n", blockchain.CalculateTotalAmount("my_blockchain_address"))
-	fmt.Printf("C %.1f\n", blockchain.CalculateTotalAmount("C"))
-	fmt.Printf("D %.1f\n", blockchain.CalculateTotalAmount("D"))
 }
